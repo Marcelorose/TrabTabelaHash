99eb17718e51ceae50d2a95234dc99f29ce0ca93
Marcelo Rose <marcelorose@edu.univali.br>
2018-05-18 17:20:41 -0300

    Incluir arquivo

---
+++ TabelaHash/THash.h
@@ -0,0 +1,24 @@
+#ifndef THASH_H
+#define THASH_H
+
+#define MAX 1000
+
+struct THash {
+	int TabelaHash[MAX];
+	int qtd;
+};
+
+void inicializarTHash(THash &t) {
+	t.qtd = 0;
+}
+
+void inserirTHash(THash t, int el, int instr) {
+	
+
+}
+
+
+
+
+
+#endif
