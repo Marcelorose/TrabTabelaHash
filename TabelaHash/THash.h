54ce5e2467ec0f5f160d4b9e128dd314a4f43adb
Marcelo Rose <marcelorose@edu.univali.br>
2018-05-23 15:59:37 -0300

    Concluído

--- TabelaHash/THash.h
+++ TabelaHash/THash.h
@@ -1,5 +1,8 @@
 #ifndef THASH_H
 #define THASH_H
+#include <iostream>
+
+using namespace std;
 
 #define MAX 1000
 
@@ -9,15 +12,57 @@ struct THash {
 };
 
 void inicializarTHash(THash &t) {
+	for (int i = 0; i < 1000; i++)
+	{
+		t.TabelaHash[i] = 0;
+	}
 	t.qtd = 0;
 }
 
-void inserirTHash(THash t, int el, int instr) {
-	
+bool inserirTHash(THash &t, int chave, int &instr) {
+	if (t.qtd >= MAX)
+	{
+		return false;
+	}
+	int posicao = chave % MAX;
 
+	for (int i = posicao; i < MAX; i++)
+	{
+		if (t.TabelaHash[i] == 0)
+		{
+			t.TabelaHash[i] = chave;
+			t.qtd++;
+			return true;
+		}
+		else if (t.TabelaHash[i] == chave)
+		{
+			return false;
+		}
+		else if (i == MAX - 1)
+		{
+			return false;
+		}
+		instr++;		
+	}
 }
 
-
+void pesquisa(THash &t, int chave, int &instr) {
+	int posicao = chave % MAX;
+	
+	for (int i = posicao; i < MAX; i++)
+	{
+		if (t.TabelaHash[i] == 0)
+		{
+			return;
+		}
+		else if (t.TabelaHash[i] == chave)
+		{
+			return;			
+		}		
+		instr++;
+	}	
+	return;
+}
 
 
 
